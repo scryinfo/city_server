9353e9ef74bd9b9c2e73d9e31af15abf20dc1bf3
jean <magiceye@163.com>
2018-09-17 16:12:46 +0800

    move client irrelative proto to server

---
+++ proto/server_compile_all_proto.bat
@@ -0,0 +1,3 @@
+protoc -I=./ --proto_path=./ --proto_path=../../city_proto25/ --java_out=../city/shared/src/main/java ./gaCode.proto
+protoc -I=./ --proto_path=./ --proto_path=../../city_proto25/ --java_out=../city/shared/src/main/java ./ga.proto
+pause
