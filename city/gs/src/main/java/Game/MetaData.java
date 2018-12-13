47271e0f147743b5335bb0702c45118e90a73ee9
scryjean <jean@scry.info>
2018-12-13 18:42:08 +0800

    Key lack equals

--- city/gs/src/main/java/Game/MetaData.java
+++ city/gs/src/main/java/Game/MetaData.java
@@ -290,6 +290,21 @@ class Formula {
             this.targetId = targetId;
             this.targetLv = targetLv;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Key key = (Key) o;
+            return targetId == key.targetId &&
+                    targetLv == key.targetLv &&
+                    type == key.type;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(type, targetId, targetLv);
+        }
     }
     enum Type{
         RESEARCH,
