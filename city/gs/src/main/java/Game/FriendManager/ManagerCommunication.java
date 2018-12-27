fd2e736555960808d7fc75ddbf850430e4af06b6
wengxuejian <294235885@qq.com>
2018-12-27 10:12:40 +0800

    聊天添加失败原因

--- city/gs/src/main/java/Game/FriendManager/ManagerCommunication.java
+++ city/gs/src/main/java/Game/FriendManager/ManagerCommunication.java
@@ -10,6 +10,7 @@ import com.google.common.base.Strings;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import common.Common;
 import gs.Gs;
 import gscode.GsCode;
 import org.apache.log4j.Logger;
@@ -82,7 +83,7 @@ public class ManagerCommunication
             }
             else
             {
-                GameServer.allGameSessions.get(player.id()).write(Package.fail((short) GsCode.OpCode.roleCommunication_VALUE));
+                GameServer.allGameSessions.get(player.id()).write(Package.fail((short) GsCode.OpCode.roleCommunication_VALUE, Common.Fail.Reason.highFrequency));
             }
         }
     }
@@ -98,7 +99,7 @@ public class ManagerCommunication
             if (GameDb.queryPlayer(friend_id).getBlacklist().contains(player.id())
                     || player.getBlacklist().contains(friend_id))
             {
-                GameServer.allGameSessions.get(player.id()).write(Package.fail((short) GsCode.OpCode.roleCommunication_VALUE));
+                GameServer.allGameSessions.get(player.id()).write(Package.fail((short) GsCode.OpCode.roleCommunication_VALUE, Common.Fail.Reason.notAllow));
             }
             else
             {
