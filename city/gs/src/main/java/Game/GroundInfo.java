3b6791af7281784ff21723d04c6baf37872f5f4a
wengxuejian <294235885@qq.com>
2018-12-29 17:46:44 +0800

    去掉租地周期

--- city/gs/src/main/java/Game/GroundInfo.java
+++ city/gs/src/main/java/Game/GroundInfo.java
@@ -25,7 +25,6 @@ public class GroundInfo implements Serializable {
     public int y;
 
     int rentPreDay;
-    int paymentCycleDays;
     int deposit;
     int rentDaysMin;
     int rentDaysMax;
@@ -45,10 +44,9 @@ public class GroundInfo implements Serializable {
     protected GroundInfo() {
     }
     public boolean sameAs(RentPara rentPara) {
-        return rentPara.paymentCycleDays == paymentCycleDays && rentPara.rentDaysMin == rentDaysMin && rentPara.rentDaysMax == rentDaysMax && rentPara.deposit == deposit && rentPara.rentPreDay == rentPreDay;
+        return rentPara.rentDaysMin == rentDaysMin && rentPara.rentDaysMax == rentDaysMax && rentPara.deposit == deposit && rentPara.rentPreDay == rentPreDay;
     }
     public void setBy(RentPara rentPara) {
-        this.paymentCycleDays = rentPara.paymentCycleDays;
         this.rentDays = rentPara.rentDays;
         this.rentPreDay = rentPara.rentPreDay;
         this.deposit = rentPara.deposit;
@@ -92,7 +90,6 @@ public class GroundInfo implements Serializable {
             giBuilder.setRentDaysMin(rentDaysMin);
             giBuilder.setRentDaysMax(rentDaysMax);
             giBuilder.setDeposit(deposit);
-            giBuilder.setPaymentCycleDays(paymentCycleDays);
             giBuilder.setRentPreDay(rentPreDay);
             builder.setRent(giBuilder);
         }
