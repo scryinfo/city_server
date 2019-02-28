97b88dd0520f706e3d957631bbee266bfefccf1f
2017y <251236877@qq.com>
2019-02-28 16:26:15 +0800

    奖金池报错修改（取不到session）

--- city/gs/src/main/java/Game/NpcManager.java
+++ city/gs/src/main/java/Game/NpcManager.java
@@ -95,8 +95,7 @@ public class NpcManager {
         allNpc.put(npc.id(), npc);
         //市民人数突破,市民人数达到500,发送广播给前端,包括市民数量，时间  
         if(allNpc!=null&&allNpc.size()>=500){
-        	GameSession gs = GameServer.allGameSessions.get(npc.id());
-        	gs.write(Package.create(GsCode.OpCode.cityBroadcast_VALUE,Gs.CityBroadcast.newBuilder()
+        	GameServer.sendToAll(Package.create(GsCode.OpCode.cityBroadcast_VALUE,Gs.CityBroadcast.newBuilder()
         			.setType(2)
         			.setNum(allNpc.size())
                     .setTs(System.currentTimeMillis())
