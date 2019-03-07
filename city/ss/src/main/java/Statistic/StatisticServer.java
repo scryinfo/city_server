dd1a150423bdbc24806a8c97d441ce8e1aafb6f4
2017y <251236877@qq.com>
2019-03-07 10:11:52 +0800

    (市民需求)每种商品购买的npc数量

--- city/ss/src/main/java/Statistic/StatisticServer.java
+++ city/ss/src/main/java/Statistic/StatisticServer.java
@@ -55,8 +55,8 @@ public class StatisticServer {
                 .withSchedule(CronScheduleBuilder.weeklyOnDayAndHourAndMinute(1,0,0))
                 .build());
         scheduler.scheduleJob(newJob(EverydayJob.class).build(), newTrigger()
-                .withSchedule(CronScheduleBuilder.cronSchedule("0 0 1 * * ?"))
-                .build());//每种商品购买的npc,每天凌晨1点执行 ,统计昨天的
+                .withSchedule(CronScheduleBuilder.cronSchedule("0 55 23 * * ?"))
+                .build());//每种商品购买的npc,每天晚上11点55开始执行 ,统计当天的
         scheduler.scheduleJob(newJob(PerHourJob.class).build(), newTrigger()
                 .withSchedule(CronScheduleBuilder.cronSchedule("0 55 */1 * * ?"))
                 .build());//每种商品购买的npc,每隔1个小时执行,统计前一个小时的
