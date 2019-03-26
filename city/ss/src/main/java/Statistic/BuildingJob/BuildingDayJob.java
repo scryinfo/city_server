40aa3f449399bf433f76044019ff2e6f7673d619
wengxuejian <294235885@qq.com>
2019-03-26 15:05:24 +0800

    建筑定时任务

---
+++ city/ss/src/main/java/Statistic/BuildingJob/BuildingDayJob.java
@@ -0,0 +1,43 @@
+package Statistic.BuildingJob;
+
+import Shared.LogDb;
+import Statistic.StatisticSession;
+import Statistic.SummaryUtil;
+import org.apache.log4j.Logger;
+import org.bson.Document;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.text.MessageFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static Statistic.SummaryUtil.DAY_MILLISECOND;
+
+public class BuildingDayJob implements Job
+{
+    private static final Logger LOGGER = Logger.getLogger(BuildingDayJob.class);
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException
+    {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS");
+        long nowTime = System.currentTimeMillis();
+        long todayStartTime = SummaryUtil.todayStartTime(nowTime);
+        long yestodayStartTime = todayStartTime - DAY_MILLISECOND;
+        String timeStr = formatter.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(nowTime), ZoneId.systemDefault()));
+        LOGGER.info("DayJob start execute,time = " + timeStr);
+
+        List<Document> documentList = LogDb.buildingDayIncomeSummary(yestodayStartTime, todayStartTime);
+        SummaryUtil.insertBuildingDayIncome(documentList,yestodayStartTime);
+
+        long nowTime1 = System.currentTimeMillis();
+        timeStr = formatter.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(nowTime1), ZoneId.systemDefault()));
+        LOGGER.info(MessageFormat.format("DayJob end execute, time = {0}, consume = {1} ms",
+                timeStr, nowTime1 - nowTime));
+    }
+}
