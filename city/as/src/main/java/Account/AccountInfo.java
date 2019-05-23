5e9f1cdbb88013f5cdf0998aaa9e321529792d59
wengxuejian <294235885@qq.com>
2019-05-23 17:25:38 +0800

    登录注册

--- city/as/src/main/java/Account/AccountInfo.java
+++ city/as/src/main/java/Account/AccountInfo.java
@@ -8,6 +8,7 @@ import java.util.Date;
 
 public final class AccountInfo {
 	public String name;
+	private String md5Pwd;
 	public Instant createTime;
 	public Instant freezeTime;
 	
@@ -15,17 +16,25 @@ public final class AccountInfo {
 		name = bson.getString("_id");
 		createTime = bson.getDate("ct").toInstant();
 		freezeTime = bson.getDate("ft").toInstant();
+		this.md5Pwd = bson.getString("pwd");
 	}
-	
-	AccountInfo(String accountName){
+
+	public String getMd5Pwd()
+	{
+		return md5Pwd;
+	}
+
+	AccountInfo(String accountName, String md5Pwd){
 		name = accountName;
 		createTime = Instant.now();
 		freezeTime = Instant.EPOCH;
+		this.md5Pwd = md5Pwd;
 	}
 	
 	Document toDocument(){
 		Document doc = new Document()
 				.append("_id", name)
+				.append("pwd",md5Pwd)
 				.append("ct", Date.from(createTime))
 				.append("ft", Date.from(freezeTime));
 		return doc;
