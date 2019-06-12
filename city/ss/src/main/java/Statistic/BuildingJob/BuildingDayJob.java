2ea45c26174ce29cd2634787d863a5ca8b1e6f82
wengxuejian <294235885@qq.com>
2019-06-12 11:37:54 +0800

    公会信息修改

--- city/ss/src/main/java/Statistic/BuildingJob/BuildingDayJob.java
+++ city/ss/src/main/java/Statistic/BuildingJob/BuildingDayJob.java
@@ -30,14 +30,14 @@ public class BuildingDayJob implements Job
         long todayStartTime = SummaryUtil.todayStartTime(nowTime);
         long yestodayStartTime = todayStartTime - DAY_MILLISECOND;
         String timeStr = formatter.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(nowTime), ZoneId.systemDefault()));
-        LOGGER.info("DayJob start execute,time = " + timeStr);
+        LOGGER.info("BuildingDay start execute,time = " + timeStr);
 
         List<Document> documentList = LogDb.buildingDayIncomeSummary(yestodayStartTime, todayStartTime);
         SummaryUtil.insertBuildingDayIncome(documentList,yestodayStartTime);
 
         long nowTime1 = System.currentTimeMillis();
         timeStr = formatter.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(nowTime1), ZoneId.systemDefault()));
-        LOGGER.info(MessageFormat.format("DayJob end execute, time = {0}, consume = {1} ms",
+        LOGGER.info(MessageFormat.format("BuildingDay end execute, time = {0}, consume = {1} ms",
                 timeStr, nowTime1 - nowTime));
     }
 }
