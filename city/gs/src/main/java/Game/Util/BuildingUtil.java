5cfe59832f86c61f196c7236c4c52dab29b52600
yty <1171031357@qq.com>
2019-06-20 17:01:49 +0800

    更新推广bug 、货架和仓库更新数据协议

--- city/gs/src/main/java/Game/Util/BuildingUtil.java
+++ city/gs/src/main/java/Game/Util/BuildingUtil.java
@@ -1,8 +1,10 @@
 package Game.Util;
 
-import Game.BrandManager;
-import Game.GameDb;
-import Game.Meta.*;
+import Game.Building;
+import Game.City;
+import Game.Meta.MetaBuilding;
+import Game.Meta.MetaData;
+import Game.Meta.MetaGood;
 
 import java.util.*;
 
@@ -18,38 +20,48 @@ public class BuildingUtil {
     //缓存零售店和住宅最大最小的基础品质
     private Map<Integer, Map<Integer, Integer>> maxQtyMap = new HashMap<>();
 
+    //初始化最大最小值品质
     public void init(){
         int apartQty=0;
         int retailQty=0;
-        List<Integer> araptmentWorkers = new ArrayList<>();
-        List<Integer> retailShopWorkers = new ArrayList<>();
-        for (MetaApartment a : MetaData.getApartment().values()) {
-            araptmentWorkers.add(a.workerNum);
-            if(apartQty==0)
-                apartQty = a.qty;
-        }
-        for (MetaRetailShop r : MetaData.getRetailShop().values()) {
-            retailShopWorkers.add(r.workerNum);
-            if(retailQty==0)
-                retailQty = r.qty;
-        }
-        Map<Integer, Integer> apartMap = getMaxOrMinQty(apartQty, araptmentWorkers);
+        //初始基础品质
+        apartQty=new ArrayList<>(MetaData.getApartment().values()).get(0).qty;
+        retailQty=new ArrayList<>(MetaData.getRetailShop().values()).get(0).qty;
+        //初始全城最大最小的建筑规模人数
+        List<Integer> apartmentWorkers = updateBuildingNumByType(MetaBuilding.APARTMENT);
+        List<Integer> retailShopWorkers=updateBuildingNumByType(MetaBuilding.RETAIL);
+        Map<Integer, Integer> apartMap = getMaxOrMinQty(apartQty, apartmentWorkers);
         Map<Integer, Integer> retailMap = getMaxOrMinQty(retailQty, retailShopWorkers);
         maxQtyMap.put(MetaBuilding.APARTMENT, apartMap);
         maxQtyMap.put(MetaBuilding.RETAIL, retailMap);
     }
 
+    public List<Integer> updateBuildingNumByType(int type){
+        List<Integer> list = new ArrayList<>();
+        Set<Building> building = City.instance().typeBuilding.getOrDefault(type,new HashSet<>());
+        building.forEach(b->{
+            if(!list.contains(b.getWorkerNum())) {
+                list.add(b.getWorkerNum());
+            }
+        });
+        return list;
+    }
 
     private Map<Integer, Integer> getMaxOrMinQty(int qty,List<Integer> list){
         Map<Integer, Integer> map = new HashMap<>();
-        Integer max = Collections.max(list);
-        Integer min = Collections.min(list);
+        Integer max = 0;
+        Integer min =0;
+        if(!list.isEmpty()) {
+            max=Collections.max(list);
+            min=Collections.min(list);
+        }
         map.put(MAX, qty * max);
         map.put(MIN, qty * min);
         return map;
     }
 
     public Map<Integer,Integer> getMaxOrMinQty(int type){
+        System.out.println("全城最大和最小基础品质为："+maxQtyMap.get(type));
         return maxQtyMap.get(type);
     }
     /*获取最大最小品牌值*/
